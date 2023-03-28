6de311bdc3d56d0874d45ec99705054b29ea77cb
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2023-03-28 13:14:20 +0900

    [Gold I] Title: 외판원 순회, Time: 1220 ms, Memory: 46676 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -1,35 +1,27 @@
 import sys
 input = sys.stdin.readline
-IMPOSSIBLE = 10**9
 
-def func(start, visited):
-    if visited == (1 << N) - 1:
-        if W[start][0]:
-            dp[start][visited] = W[start][0]
+def func(curr, visited):
+    if visited + 1 == (1 << N):
+        if W[curr][0]:
+            dp[curr][visited] = W[curr][0]
         else:
-            dp[start][visited] = IMPOSSIBLE
-        return dp[start][visited]
-        
-    if dp[start][visited] != float('inf'):
-        return dp[start][visited]
-
+            dp[curr][visited] = float('inf')
+        return dp[curr][visited]
+    if dp[curr][visited] != -1:
+        return dp[curr][visited]    
+    dp[curr][visited] = float('inf')
     for next in range(1, N):
-        if not W[start][next]:
+        if not W[curr][next]:
             continue
         if visited & (1 << next):
             continue
-        next_cost = func(next, visited | (1 << next))
-        if next_cost == IMPOSSIBLE:
-            continue
-        else:
-            dp[start][visited] = min(dp[start][visited], next_cost + W[start][next])
-    if dp[start][visited] == float('inf'):
-        dp[start][visited] = IMPOSSIBLE
-    return dp[start][visited]
+        dp[curr][visited] = min(dp[curr][visited], W[curr][next] + func(next, visited | (1 << next)))    
+    return dp[curr][visited]
 
 N = int(input())
 W = []
-dp = [[float('inf')] * (1 << N) for _ in range(N)]
-for i in range(N):
+dp = [[-1] * (1 << N) for _ in range(N)]
+for _ in range(N):
     W.append(list(map(int, input().split())))
-print(func(0, 1))
+print(func(0,1))
