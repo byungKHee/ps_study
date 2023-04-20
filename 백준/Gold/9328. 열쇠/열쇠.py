e5f7ec019fcaa85b6d3735014008270b238f66d1
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2023-04-20 16:20:27 +0900

    [Gold I] Title: 열쇠, Time: 524 ms, Memory: 34288 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.py
+++ 백준/Gold/9328. 열쇠/열쇠.py
@@ -2,13 +2,9 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
+# bfs direction
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-
-def loop():
-    while True:
-        if not search():
-            break
 
 def search():
     global keys, answer
@@ -22,29 +18,30 @@ def search():
         for i in range(4):
             nx = cx + dx[i]
             ny = cy + dy[i]
+            # range check
             if nx < 0 or nx >= N+2 or ny < 0 or ny >= M+2:
                 continue
-
+            # visited check
             if visited[nx][ny]:
                 continue
             if arr[nx][ny] == '.':
                 Q.append([nx, ny])
                 visited[nx][ny] = True
-
+            # target find
             if arr[nx][ny] == '$':
                 answer += 1
                 arr[nx][ny] = '.'
                 change = True
                 Q.append([nx,ny])
                 visited[nx][ny] = True
-
+            # key find
             elif isKey(nx,ny):
                 keys.append(arr[nx][ny])
                 arr[nx][ny] = '.'
                 change = True
                 Q.append([nx,ny])
                 visited[nx][ny] = True
-
+            # door find
             elif isDoor(nx,ny):
                 if openDoor(nx, ny):
                     arr[nx][ny] = '.'
@@ -62,19 +59,32 @@ def openDoor(x,y):
 def isKey(x,y):
     return 97 <= ord(arr[x][y]) <= 122
 
-for testCase in range(int(input())):
+def setParameter():
+    global N, M, arr, target, keys
     N, M = map(int, input().split())
     arr = [['.' for _ in range(M+2)] for _ in range(N+2)]
-    keys = []
-    target = []
-    answer = 0
     for i in range(N):
         temp = list(input().rstrip())
         for j in range(M):
             arr[i+1][j+1] = temp[j]
             if arr[i+1][j+1] == '$':
-                target.append([i+1, j+1])
+                target.append([i+1, j+1])        
     keys = list(input().rstrip())
     if keys[0] == 0: keys = []
-    loop()
+
+for testCase in range(int(input())):
+    # parameter declaration
+    N = 0
+    M = 0
+    arr = []
+    keys = []
+    target = []
+    answer = 0
+    # setting parameter
+    setParameter()
+    # main loop
+    while True:
+        change = search()
+        if not change:
+            break
     print(answer)
