122b716d189224203952d9ceddad6e53e8ce2aa1
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2023-05-17 16:39:09 +0900

    [Gold III] Title: 개미굴, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.py
+++ 백준/Gold/14725. 개미굴/개미굴.py
@@ -1,38 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-class Node:
-    def __init__(self,key):
-        self.key = key
-        self.children = {}
+Tree = {}
 
-class Tree:
-    def __init__(self):
-        self.root = Node(None)
+def make(curr, data):
+    if not data:
+        return
+    if data[0] not in curr:
+        curr[data[0]] = {}
+    make(curr[data[0]], data[1:])
     
-    def printNode(self, depth, curr):
+def printTree(curr, depth):
+    for key in sorted(curr.keys()):
         print('--' * depth, end = '')
-        print(curr.key)
-        if curr.children:
-            for idx in sorted(curr.children.keys()):
-                self.printNode(depth+1, curr.children[idx])      
+        print(key)
+        printTree(curr[key], depth+1)
 
-    def printTree(self):
-        for idx in sorted(self.root.children.keys()):
-            self.printNode(0, self.root.children[idx])
-
-    def make(self, data):
-        currNode = self.root
-        for key in data:
-            if key not in currNode.children:
-                currNode.children[key] = Node(key)
-            currNode = currNode.children[key]
-        
 N = int(input())
 arr = []
 for _ in range(N):
     arr.append(input().rstrip().split()[1:])
-T = Tree()
 for data in arr:
-    T.make(data)
-T.printTree()
+    make(Tree, data)
+printTree(Tree, 0)
