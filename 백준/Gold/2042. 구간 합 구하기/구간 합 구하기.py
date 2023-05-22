e8c1ef8962a7a1b6bebd82185efbf2bd96a80572
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2023-05-22 15:04:48 +0900

    [Gold I] Title: 구간 합 구하기, Time: 896 ms, Memory: 127572 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -1,46 +1,55 @@
 import sys
-import math
 input = sys.stdin.readline
 
-def make(node, start, end):
-    if start + 1 == end:
-        tree[node] = num[start]
-        leafIdx[start] = node
-        return tree[node]
-    else:
-        mid = (start + end) // 2
-        left = make(node*2, start, mid)
-        right = make(node*2+1, mid, end)
-        tree[node] = left + right
-        return tree[node]
+######################################
+##### segment tree final version #####
+######################################
 
+def findSize():
+    s = 0
+    while 2**s < 2*N:
+        s += 1
+    return 2**s
+
+def makeTree():
+    tree = [0] * Size
+    for i in range(N):
+        tree[Size//2 + i] = data[i]
+    for node in range(Size//2-1, 0, -1):
+        tree[node] = tree[node*2] + tree[node*2+1]
+    return tree
+
+# [left, right) form
 def subSum(node, nodeL, nodeR, targetL, targetR):
     if nodeR <= targetL or targetR <= nodeL:
         return 0
     if targetL <= nodeL and nodeR <= targetR:
-        return tree[node]
+        return segTree[node]
     mid = (nodeL + nodeR) // 2
     left = subSum(node*2, nodeL, mid, targetL, targetR)
     right = subSum(node*2+1, mid, nodeR, targetL, targetR)
     return left + right
 
-def update(idx, val):
-    node = leafIdx[idx]
-    tree[node] = val
+# index start from 0
+def update(i, num):
+    node = Size//2 + i
+    segTree[node] = num
     while node > 1:
-        node //= 2
-        tree[node] = tree[node*2] + tree[node*2+1]
+        node = node // 2
+        segTree[node] = segTree[node*2] + segTree[node*2+1]
 
+# input data
 N, M, K = map(int, input().split())
-num = []
-tree = [0] * 3000000
-leafIdx = [0] * N
-for _ in range(N):
-    num.append(int(input()))
-make(1, 0, N)
-for i in range(M+K):
+data = [int(input()) for _ in range(N)]
+
+# build segment tree
+Size = findSize()
+segTree = makeTree()
+
+# main process
+for _ in range(M+K):
     a,b,c = map(int, input().split())
     if a == 1:
         update(b-1, c)
     else:
-        print(subSum(1, 0, N, b-1, c))
+        print(subSum(1, 0, Size//2, b-1, c))
