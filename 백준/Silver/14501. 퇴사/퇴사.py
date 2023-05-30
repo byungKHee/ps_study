abae5effab3a3576151659a436bd98d579025be1
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2023-05-30 14:20:30 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,13 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-def func(day):
-    rnt = 0
-    for i in range(day, N):
-        if i + arr[i][0] <= N:
-            rnt = max(rnt, arr[i][1] + func(i+arr[i][0]))
-    return rnt
-    
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
-print(func(0))
+dp = [0] * (N+1)
+for day in range(N):
+    for i in range(day, N):
+        if i + arr[i][0] <= N:
+            dp[i + arr[i][0]] = max(dp[i + arr[i][0]], dp[day] + arr[i][1])
+print(max(dp))
