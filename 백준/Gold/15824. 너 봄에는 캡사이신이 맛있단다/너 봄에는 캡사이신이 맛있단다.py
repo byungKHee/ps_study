5030cbcd9845a840cf33d8e9ab585476ff527aec
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2023-06-07 16:41:52 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,19 +1,6 @@
 import sys
-sys.setrecursionlimit(10**4)
 input = sys.stdin.readline
 DIVIDE = 1_000_000_007
-
-def pow(n):
-    if dp[n] != -1:
-        return dp[n]
-    if n % 2 == 0:
-        half = pow(n//2)
-        dp[n] = half * half % DIVIDE
-        return dp[n]
-    else:
-        half = pow(n//2)
-        dp[n] = half * half * 2 % DIVIDE
-        return dp[n]
 
 N = int(input())
 arr = list(map(int, input().split()))
@@ -23,6 +10,6 @@ dp[0] = 1
 dp[1] = 2
 answer = 0
 for i in range(N):
-    answer += (arr[i] * (pow(i) - 1)) - (arr[i] * (pow(N-i-1) - 1))
+    answer += (arr[i] * (pow(2,i) - 1)) - (arr[i] * (pow(2,N-i-1) - 1))
     answer %= DIVIDE
 print(answer)
