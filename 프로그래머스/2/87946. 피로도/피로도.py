21ee38ef4ec0ed02e1b7a01168d19ec87e795d85
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2025-10-11 00:06:43 +0900

    [level 2] Title: 피로도, Time: 369.16 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,20 @@
-from itertools import permutations
+from copy import deepcopy
+
+answer = -1
+arr = []
+def dfs(cnt, curr, visited):
+    global arr, answer
+    answer = max(answer, cnt)
+    for i in range(len(arr)):
+        if i in visited: continue
+        if arr[i][0] <= curr:
+            next = deepcopy(visited)
+            next.add(i)
+            dfs(cnt + 1, curr - arr[i][1], next)
+
 def solution(k, dungeons):
-    answer = -1
-    for arr in permutations(dungeons, len(dungeons)):
-        cnt = 0
-        health = k
-        for require, cost in arr:
-            if require <= health:
-                cnt += 1
-                health -= cost
-            else:
-                break
-        answer = max(answer, cnt)
+    global arr
+    arr = dungeons
+    dfs(0, k, set())
     
     return answer
