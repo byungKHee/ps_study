1210a1afdebc70cdbfd1672ffae5f167682029bc
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2025-11-03 13:29:19 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT b.TITLE,
+    b.BOARD_ID,
+    r.REPLY_ID,
+    r.WRITER_ID,
+    r.CONTENTS,
+    DATE_FORMAT(r.CREATED_DATE, "%Y-%m-%d") as CREATED_DATE
+from USED_GOODS_BOARD as b, USED_GOODS_REPLY as r
+where b.BOARD_ID = r.BOARD_ID
+    and DATE_FORMAT(b.CREATED_DATE, "%Y-%m") = '2022-10'
+order by r.CREATED_DATE, b.TITLE
