12004fec8deb341e90b8038b6c34a10d9093892d
Byunghee Kang <80045655+byungKHee@users.noreply.github.com>
2025-11-11 18:20:58 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using ll = long long;
+
+using namespace std;
+
+long long solution(int n) {
+
+    ll dp[2002];
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for(int i = 2; i <= n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    
+    return dp[n];
+}
